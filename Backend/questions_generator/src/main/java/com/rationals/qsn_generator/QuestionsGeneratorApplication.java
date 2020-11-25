dfbce3866f28e00f0c66cd38e4c0af8b10e4f5b6
prashanthcharla1234 <prashanthcharla1234@gmail.com>
2020-11-25 21:31:26 +0530

    Added folder structure - UI(angular) Backend(Spring Boot)

---
+++ Backend/questions_generator/src/main/java/com/rationals/qsn_generator/QuestionsGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.rationals.qsn_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionsGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionsGeneratorApplication.class, args);
+	}
+
+}
---
+++ Backend/questions_generator/src/test/java/com/rationals/qsn_generator/QuestionsGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rationals.qsn_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
